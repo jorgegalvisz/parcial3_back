dbac79996f36b8c5e6e2a98e185105c130065294
jorgegalvisz <jorgeenrique0523@gmail.com>
2025-06-04 20:35:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const io = socketIo(server);
 app.use(cors());
 app.use(express.json());
 
-// Conexión a MongoDB
+// Conexión a MongoDB jjjjjjjjjjjjjjjjjj
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
